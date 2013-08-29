6a7aed0ea80cc44197401a14ae328af075ece7b2
Patricia Estridge <patricia.j.estridge@gmail.com>
2013-08-29 19:48:11 +0100

    debugging seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,57 @@
 
 require 'faker'
 
-rand(4..10).times do
-  password = Faker::Lorem.characters(10)
-  u = User.new(
-    name: Faker::Name.name, 
-    email: Faker::Internet.email, 
-    password: password, 
-    password_confirmation: password)
-  u.skip_confirmation!
-  u.save
+# Create 15 topics
+  topics = []
+  15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+  end
 
   rand(5..12).times do
+    topic = topics.first # getting the first topic here
     p = u.posts.create(
+      topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
 
-    rand(3..7).times do
-      p.comments.create(
-        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
-    end
-  end
-end
+    topics.rotate! # add this line to move the first topic to the last, so that posts get assigned to different topics.
 
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(email: 'patricia.j.estridge@gmail.com', password: 'helloworld', password_confirmation: 'helloworld')
+    # comments...
+    rand(3..10).times do
+    p.comments.create(body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
+  end
+  end
+
+u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
