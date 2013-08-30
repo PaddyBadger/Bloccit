a423abfcd4d4fc03029be5479e5cb402f6edfd7e
Patricia Estridge <patricia.j.estridge@gmail.com>
2013-08-30 12:13:47 +0100

    Email change

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "app17767690@heroku.com"
+  config.mailer_sender = "patricia.j.estridge@gmail.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
