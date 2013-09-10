836ad51289d8a0881a6bab206df18291ae4962df
Patricia Estridge <patricia.j.estridge@gmail.com>
2013-09-10 10:50:40 +0100

    Scoped Topics for privacy

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,7 @@ class Topic < ActiveRecord::Base
   attr_accessible :description, :name, :public
   has_many :posts
   has_many :posts, dependent: :destroy
+
+  scope :public, lambda { |user| user ? scoped : where(public: true) }
   
 end
